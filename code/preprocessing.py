65a5dfb6061ac2b87b1808e0200ed01182cea528
Caasimov <38560788+Caasimov@users.noreply.github.com>
2024-03-14 18:03:48 +0100

    Refactored preprocessing.py to use HDF5 files

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -8,88 +8,115 @@ def kalman_filter():
     rk = ExtendedKalmanFilter(dim_x=3, dim_z=1)
     dt = 0.05
 
-
-def load_log_file(fname):
-    '''
-    Load a log file into a pandas dataframe
-    '''
-    df = pd.read_csv(fname, sep='\s+', engine='python', header=None)
-    return df
-
-def load_hdf5_file(fname):
+def hdf5_to_df(fname, dof):
     '''
     Load a hdf5 file into a pandas dataframe
     '''
-    with h5py.File(fname, 'r') as f:
-        data = f['data/commanded/data']
-        df = pd.DataFrame(data)
-    print(df.head())
-
-def clean_data(fname, index_lst):
-    '''
-    Clean data from .log files and return a pandas dataframe
-    '''
-    df = pd.read_csv(fname, sep='\s+', engine='python', header=None)
-
-    # Ensure constant measurement offsets & input lag is corrected
-    coef = 1
-    for dof, idx_pair in index_lst.items():
-        if dof == 'z':
-            # The z-values for the measured signal is flipped for some reason
-            coef = -1
-        else:
-            coef = 1
-        ### START BREAK-OUT LOGIC: REMOVE ONCE ROT CMD DATA CLARIFIED ###
-        if dof == 'phi':
-            break
-        ### END BREAK-OUT LOGIC: REMOVE ONCE ROT CMD DATA CLARIFIED ###
-        
-        # Iteratively correct offset and lag since they are interdependent
-        prev_offset = prev_lag = float('inf')
-        while True:
-            offset = find_offset(df, idx_pair)
-            df.loc[:, idx_pair[1]] = coef*(df.loc[:, idx_pair[1]] + offset)
-
-            # Find the lag in terms of a row difference in input/output functions
-            lag, cmd_row_idx, mes_row_idx = find_lag(df, idx_pair)
-            idx_lag = mes_row_idx-cmd_row_idx
-
-
-            # Shift measurement signal up to account for input lag
-            df.loc[:, idx_pair[1]] = df.loc[:, idx_pair[1]].shift(-idx_lag)
-
-            # Tolerance set to 1e-5 for both offset and lag
-            if abs(offset-prev_offset) < 1e-5 and abs(lag-prev_lag) < 1e-5:
-                break
-
-            prev_offset = offset
-            prev_lag = lag
-            print(f"Offset: {offset}, Lag: {lag}, idx_lag: {idx_lag}")
+    paths_cmd = [
+        'data/commanded/tick',
+        f'data/commanded/data/{dof}',
+        f'data/commanded/data/{dof}dot',
+        f'data/commanded/data/{dof}dotdot'
+        ]
+    paths_mes = [
+        'data/measured/tick',
+        'data/measured/data/actual_pos'
+        ]
     
-    # Remove NaN values that would come from the measurement shift
+    index_match = {"x": 0, "y": 1, "z": 2, "phi": 3, "theta": 4, "psi": 5}
+    column_names_cmd = ['t', 'pos_cmd', 'vel_cmd', 'acc_cmd']
+    column_names_mes = ['t', 'pos_mes']
+    
+    with h5py.File(fname, 'r') as f:
+        # Create a list of dataframes from the paths
+        dfs_cmd = [pd.DataFrame(f[path][()], dtype=np.float64) for path in paths_cmd]
+        dfs_mes = [
+            pd.DataFrame(f[paths_mes[0]][()], dtype=np.float64),
+            pd.DataFrame(f[paths_mes[1]][()][:, index_match[dof]], dtype=np.float64)
+            ]
+    
+    # Concatenate the dataframes along the columns axis
+    df_cmd = pd.concat(dfs_cmd, axis=1)
+    df_mes = pd.concat(dfs_mes, axis=1)
+    
+    # Rename the columns
+    df_cmd.columns = column_names_cmd
+    df_mes.columns = column_names_mes
+    
+    # Merge the dataframes on the time column
+    merged_df = pd.merge(df_cmd, df_mes, on='t', how='inner')
+    
+    # Adjust for inverted z-axis
+    if dof == 'z':
+        merged_df['pos_mes'] = merged_df['pos_mes'] * -1
+    
+    return merged_df
+
+def clean_data(df):
+    '''
+    Remove rows with NaN values and reset index.
+    '''
     df.dropna(inplace=True)
     df.reset_index(drop=True, inplace=True)
     
-    # ASK RENE: ISSUE WITH PMD CMD SIGNAL IDX KEY >= 41  (PHI, THETA, PSI), COL = 0
-    # Filter out columns full of zeroes        
-    #df = df.loc[:, ~(df == 0).all()]
-       
-    return df
+def find_derivatives(df):
+    '''
+    Compute derivative of a specified DoF using forward difference formula.
+    '''
+    df['vel_mes'] = (df['pos_mes'].shift(-1) - df['pos_mes']) / (df['t'].shift(-1) - df['t'])
+    df['acc_mes'] = (df['vel_mes'].shift(-1) - df['vel_mes']) / (df['t'].shift(-1) - df['t'])
+    
+    # Remove rows with NaN values and reset index
+    clean_data(df)
 
-def find_offset(df, idx_pair):
+def find_offset(df):
     '''
     Determine positional/rotational system offset for a specified DoF between commanded and measured data.
     '''
-    avg_offset = 0
-    
-    # Average out positional offset forthe first three datapoints
-    for i in range(1,4):
-        avg_offset += (-1*df.loc[i, idx_pair[0]]) - df.loc[i, idx_pair[1]]
-    avg_offset = avg_offset/3
-    
-    return avg_offset
+    return df.loc[0, 'pos_mes'] - df.loc[0, 'pos_cmd']
 
-def plot_dof(df, idx_pair, interval=None):
+def find_lag(df):
+    '''
+    Determine the fixed delay between input and output signals using the commanded and measured velocities using cross-correlation.
+    '''    
+    # Determine cross correlation
+    cross_corr = np.correlate(df['vel_cmd'], df['vel_mes'], mode='full')
+    
+    idx_lag = abs(cross_corr.argmax() - len(df['vel_mes']) + 1) 
+    time_lag = df.loc[idx_lag, 't'] - df.loc[0, 't']
+    
+    # Actual lag value in terms of time returned along with the position of the zero on the cmd and mes columns
+    return time_lag, idx_lag 
+
+def preprocess(df, freq_sample=100):
+    '''
+    Clean the dataframe for direct analysis.
+    '''
+    
+    # Determine time scaling factor
+    delta_t = np.float64(1/freq_sample)
+    scaling_factor = np.float64(delta_t/(df.loc[1, 't']-df.loc[0, 't']))
+    
+    # Adjust time column to be in seconds
+    df['t'] = df['t']*scaling_factor
+
+    # Add measured velocity and acceleration using forward difference formula
+    find_derivatives(df)
+    
+    # Adjust for amplitude offset between cmd and mes signals
+    df['pos_mes'] = df['pos_mes'] - find_offset(df)
+    
+    # Adjust for lag between cmd and mes signals
+    _, idx_lag = find_lag(df)
+    df['pos_mes'] = df['pos_mes'].shift(-idx_lag)
+    df['vel_mes'] = df['vel_mes'].shift(-idx_lag)
+    df['acc_mes'] = df['acc_mes'].shift(-idx_lag)
+    clean_data(df)
+    
+    # Set t_0 to 0
+    df['t'] = df['t'] - df.loc[0, 't']
+
+def plot_dof(df, dof, file_type, interval=None):
     '''
     Plot position, velocity & acceleration corresponding to a specified DoF for both commanded and measured data. 
     '''
@@ -97,30 +124,30 @@ def plot_dof(df, idx_pair, interval=None):
         interval = list(range(0, df.shape[0]))
 
     # Numerical derivatives of var_1
-    vel_1 = df.loc[interval, idx_pair[0]+6]
-    acc_1 = df.loc[interval, idx_pair[0]+12]
+    vel_1 = df.loc[interval, 'vel_cmd']
+    acc_1 = df.loc[interval, 'acc_cmd']
     
     # Numerical derivatives of var_2
-    vel_2 = np.gradient(df.loc[interval, idx_pair[1]], df.loc[interval, 0])*10**4
-    acc_2 = np.gradient(vel_2, df.loc[interval, 0])*10**4
+    vel_2 = df.loc[interval, 'vel_mes']
+    acc_2 = df.loc[interval, 'acc_mes']
     
     fig, axs = plt.subplots(3, 1, figsize=(10,10))
 
     # Position subplot
-    axs[0].plot(df.loc[interval, 0], df.loc[interval, idx_pair[0]], label="CMD pos", color='navy')
-    axs[0].plot(df.loc[interval, 0], df.loc[interval, idx_pair[1]], label="MES pos", color='darkred')
+    axs[0].plot(df.loc[interval, 't'], df.loc[interval, 'pos_cmd'], label="CMD pos", color='navy')
+    axs[0].plot(df.loc[interval, 't'], df.loc[interval, 'pos_mes'], label="MES pos", color='darkred')
     axs[0].set_title('Position')
     axs[0].legend()
 
     # Velocity subplot
-    axs[1].plot(df.loc[interval, 0], vel_1, label="CMD vel", color='dodgerblue')
-    axs[1].plot(df.loc[interval, 0], vel_2, label="MES vel", color='red')
+    axs[1].plot(df.loc[interval, 't'], vel_1, label="CMD vel", color='dodgerblue')
+    axs[1].plot(df.loc[interval, 't'], vel_2, label="MES vel", color='red')
     axs[1].set_title('Velocity')
     axs[1].legend()
 
     # Acceleration subplot
-    axs[2].plot(df.loc[interval, 0], acc_1, label="CMD acc", color='lightskyblue')
-    axs[2].plot(df.loc[interval, 0], acc_2, label="MES acc", color='lightcoral')
+    axs[2].plot(df.loc[interval, 't'], acc_1, label="CMD acc", color='lightskyblue')
+    axs[2].plot(df.loc[interval, 't'], acc_2, label="MES acc", color='lightcoral')
     axs[2].set_title('Acceleration')
     axs[2].legend()
 
@@ -130,50 +157,23 @@ def plot_dof(df, idx_pair, interval=None):
 
     # Adjust the layout
     plt.tight_layout()
+    
+    # Save the figure
+    plt.savefig(f'plots/{file_type}--{dof}.png')
+    
+    # Show the figure
     plt.show()
+    
+file_dir = {
+    "AGARD-AR-144_A": "data/hdf5/motionlog-20240301_133202.hdf5",
+    "AGARD-AR-144_B+E": "data/hdf5/motionlog-20240301_141239.hdf5",
+    "MULTI-SINE": "data/hdf5/motionlog-20240301_144109.hdf5",
+    "BUMP": "data/hdf5/motionlog-20240301_150040.hdf5",
+    "PMD": "data/hdf5/motionlog-20240301_150320.hdf5",
+}
 
-def find_lag(df, idx_pair):
-    '''
-    Determine the fixed delay between input and output signals using the commanded and measured velocities.
-    '''
-    pos_cmd = df.loc[:, idx_pair[0]]
-    pos_mes = df.loc[:, idx_pair[1]]
-    cmd_zero = False
-
-    # Check if pos_cmd and pos_mes are full of zeroes
-    if not np.any(pos_cmd) or not np.any(pos_mes):
-        print("Error: Degree of Freedom is full of zeroes.")
-        return None, None, None
-
-    for i in range(len(pos_cmd)-1):
-        # Ticker starts once zero position identified on cmd signal
-        if (pos_cmd[i] * pos_cmd[i+1] < 0) and not cmd_zero:
-            cmd_zero = True
-            ticker_start = df.loc[i, 0]
-            cmd_row_idx = i
-            print(f"CMD zero @ {df.loc[i, 0]}\nRows: {i}-{i+1}")
-            
-        # Ticker stops once zero position identified on mes signal
-        if (pos_mes[i] * pos_mes[i+1] < 0)  and cmd_zero:
-            ticker_end = df.loc[i, 0]
-            mes_row_idx = i
-            print(f"MES zero @ {df.loc[i, 0]}\nRows: {i}-{i+1}")
-            break
-
-    # If no zero crossing was found, return None
-    if not cmd_zero:
-        print("Error: No zero crossing found.")
-        return None, None, None
-
-    # Actual lag value in terms of time returned along with the position of the zero on the cmd and mes columns
-    return ticker_end-ticker_start, cmd_row_idx, mes_row_idx 
-
-
-
-index_match = {"x": (38, 74), "y": (39, 75), "z": (40, 76), "phi": (41, 77), "theta": (42,78), "psi": (43, 79)}
-
-data = clean_data('data/log/motion240301-pmd.log', index_match)
-data_range  = list(range(10,528))
-#data_range = list(range(5000, 6000))
-plot_dof(data, index_match['z'])
-load_hdf5_file('data/hdf5/motionlog-20240301_133202.hdf5')
+dof = 'z'
+file_type = 'PMD'
+df_z = hdf5_to_df(file_dir[file_type], dof)
+preprocess(df_z)
+plot_dof(df_z, dof, file_type)
