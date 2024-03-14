a9922a217f1b5e01bae5c269e5597d39344106a1
amalugani <a.malugani@student.tudelft.nl>
2024-03-14 08:47:04 +0100

    Update Json code

--- code/Json code.py
+++ code/Json code.py
@@ -10,11 +10,23 @@ import matplotlib.pyplot as plt
     # Load the JSON data
     #data = json.load(f)
     
-with open('C:\\Users\\auror\\Documents\\TU Delft\\DARE\\Git\\reversal-bump-test-new\\data\\json\\srs-agard144d.json', 'r') as f:
+#with open('C:\\Users\\auror\\Documents\\TU Delft\\DARE\\Git\\reversal-bump-test-new\\data\\json\\srs-agard144d.json', 'r') as f:
     # Load the JSON data
-    data = json.load(f)
+    #data = json.load(f)
 
 #with open('C:\\Users\\auror\\Documents\\TU Delft\\DARE\\Git\\reversal-bump-test-new\\data\\json\\srs-agard144e.json', 'r') as f:
+    # Load the JSON data
+    #data = json.load(f)
+
+#with open('C:\\Users\\auror\\Documents\\TU Delft\\DARE\\Git\\reversal-bump-test-new\\data\\json\\srs-test-motion-sines1.json', 'r') as f:
+    # Load the JSON data
+    #data = json.load(f)
+
+#with open('C:\\Users\\auror\\Documents\\TU Delft\\DARE\\Git\\reversal-bump-test-new\\data\\json\\srs-test-motion-sines2.json', 'r') as f:
+    # Load the JSON data
+    #data = json.load(f)
+
+#with open('C:\\Users\\auror\\Documents\\TU Delft\\DARE\\Git\\reversal-bump-test-new\\data\\json\\srs-test-motion-sines3.json', 'r') as f:
     # Load the JSON data
     #data = json.load(f)
     
@@ -22,34 +34,41 @@ with open('C:\\Users\\auror\\Documents\\TU Delft\\DARE\\Git\\reversal-bump-test-
 extracted_data = [
     [move["move"]["profile"]["Tfade"],
     move["move"]["profile"]["Ttotal"],
-    move["move"]["profile"]["omg"][0],
-    move["move"]["profile"]["gain"][0]]
+    move["move"]["profile"]["omg"],
+    move["move"]["profile"]["gain"],
+    move["move"]["profile"]["phi0"]]
     for move in data["moves"] if "profile" in move["move"] and "FadedSineProfile" in move["move"]["profile"]["type"]
 ]
 
-print(extracted_data)
+#print(extracted_data)
 
-def create_sine(Tfade, Ttotal, omg, gain):
+def create_sine(Tfade, Ttotal, omg, gain, phi0):
     x_val = []
     t_val = []
     t_sine = Ttotal - 2*Tfade
-    print(omg)
-    print(gain)
     for t in np.arange(0, t_sine, 0.01):
-        x = omg * np.sin(gain*t)
+        x = gain * np.sin(omg*t + phi0)
         t_val.append(t)
         x_val.append(x)
     return t_val, x_val
 
-#print(len(extracted_data))
 for i in range(0, len(extracted_data), 6):
-    #print(extracted_data[i][2])
-    #print(extracted_data[i][3])
-    t_values, sine_function = create_sine(extracted_data[i][0], extracted_data[i][1], extracted_data[i][2], extracted_data[i][3])
-    # Plot the sine function
-    plt.plot(t_values, sine_function)
-    plt.title('Plot of Sine Function')
-    plt.xlabel('dt')
-    plt.ylabel('sin(x)')
-    plt.grid(True)
-    plt.show()
+    if len(extracted_data[i][2]) == 1:
+        t_values, sine_function = create_sine(extracted_data[i][0], extracted_data[i][1], extracted_data[i][2][j], extracted_data[i][3][0], extracted_data[i][4][0])
+    else:
+        sine_functions = [[]] * len(extracted_data[0][2])
+        sine_function = []
+        for j in range(len(extracted_data[i][2])):
+            t_values, sine_functions[j] = create_sine(extracted_data[i][0], extracted_data[i][1], extracted_data[i][2][j], extracted_data[i][3][j], extracted_data[i][4][j])
+            if j == 0:
+                sine_function.append(sine_functions[j])
+            else:
+                for k in range(len(sine_function[0])):
+                    sine_function[0][k] = sine_function[0][k] + sine_functions[j][k]
+        # Plot the sine function
+        plt.plot(t_values, sine_function[0], 'r')
+        plt.title('Plot of Sine Function')
+        plt.xlabel('dt')
+        plt.ylabel('sin(x)')
+        plt.grid(True)
+        plt.show()
