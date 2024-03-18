6877aac38157e0c6e11513f358f0e58ce13471e1
amalugani <a.malugani@student.tudelft.nl>
2024-03-18 10:05:06 +0100

    Update Json_code.py

--- code/Json_code.py
+++ code/Json_code.py
@@ -30,11 +30,22 @@ def create_sine(dt, Tfade, Ttotal, omg, gain, phi0, axis):
             x_val.append(x)
     return t_val, x_val
 
+def time_conversion(extracted_data):
+    time_stamps = []
+    for i in range(0, len(extracted_data)):
+        if len(extracted_data[i][3]) == 1:
+            time_stamps.append([extracted_data[i][1]+extracted_data[i][0], extracted_data[i][2]+extracted_data[i][0]- extracted_data[i][1]])     
+        else:   
+            time_stamps.append([extracted_data[i][1]+extracted_data[i][0], extracted_data[i][2]+extracted_data[i][0]- extracted_data[i][1]])
+    time_stamps =  [[round(entry * 10**-4, 6) for entry in sublist] for sublist in time_stamps]
+
+    return time_stamps
+
 file_path = 'C:\\Users\\auror\\Documents\\TU Delft\\DARE\\Git\\reversal-bump-test-new\\data\\json\\srs-agard144a.json'
 #file_path = 'data/json/srs-agard144b.json'
 #file_path = 'data/json/srs-agard144d.json' 
 #file_path = 'data/json/srs-agard144e.json'
-#file_path = 'data/json/srs-test-motion-sines1.json'
+#file_path = 'C:\\Users\\auror\\Documents\\TU Delft\\DARE\\Git\\reversal-bump-test-new\\data\\json\\srs-test-motion-sines1.json'
 #file_path = 'data/json/srs-test-motion-sines2.json'
 #file_path = 'data/json/srs-test-motion-sines3.json'
 
@@ -54,7 +65,7 @@ for i in range(0, len(extracted_data)):
     if len(extracted_data[i][3]) == 1:
         combined_t_values.extend(create_sine(extracted_data[i][0], extracted_data[i][1], extracted_data[i][2], extracted_data[i][3][0], extracted_data[i][4][0], extracted_data[i][5][0], extracted_data[i][6])[0])
         combined_sine_function.extend(create_sine(extracted_data[i][0], extracted_data[i][1], extracted_data[i][2], extracted_data[i][3][0], extracted_data[i][4][0], extracted_data[i][5][0], extracted_data[i][6])[1])
-        dt_values.append([extracted_data[i][1]+extracted_data[i][0], extracted_data[i][2]+extracted_data[i][0]- extracted_data[i][1]])     
+        dt_values = time_conversion(extracted_data)
     else:
         for j in range(len(extracted_data[i][3])):
             t_values, sine_function = create_sine(extracted_data[i][0], extracted_data[i][1], extracted_data[i][2], extracted_data[i][3][j], extracted_data[i][4][j], extracted_data[i][5][j], extracted_data[i][6])
@@ -63,9 +74,10 @@ for i in range(0, len(extracted_data)):
         combined_sine_function.extend([sum(samples) for samples in zip(*sine_functions)])
         combined_t_values.extend(t_values)
 
-        dt_values.append([extracted_data[i][1]+extracted_data[i][0], extracted_data[i][2]+extracted_data[i][0]- extracted_data[i][1]])
+        dt_values = time_conversion(extracted_data)
 
-    
+        
+print(dt_values)    
 # Plot the combined sine function
 plt.plot(combined_t_values, combined_sine_function)
 plt.title('Plot of Combined Sine Functions')
